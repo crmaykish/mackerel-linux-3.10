c28a62f1ef7e65f89922304a4c30f690432af451
Colin Maykish <crmaykish@gmail.com>
2022-08-06 22:24:57 -0400

    start working on timer interrupt

--- arch/m68k/platform/68000/mackerel/config-mackerel.c
+++ arch/m68k/platform/68000/mackerel/config-mackerel.c
@@ -1,7 +1,52 @@
+#include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <asm/traps.h>
+#include <asm/io.h>
+#include <asm/machdep.h>
+#include <asm/siginfo.h>
+#include <linux/kallsyms.h>
+#include <linux/signal.h>
+#include <linux/ptrace.h>
+#include <asm/mackerel.h>
+
+static irq_handler_t timer_interrupt;
+
+static irqreturn_t mfp_timer_tick(int irq, void *dummy)
+{
+    MEM(MFP_GPDR) += 1;
+
+	return timer_interrupt(irq, dummy);
+}
+
+static struct irqaction mackerel_timer_irq = {
+	.name	 = "timer",
+	.flags	 = IRQF_DISABLED | IRQF_TIMER,
+	.handler = mfp_timer_tick,
+};
+
+void mackerel_timer_init(irq_handler_t handler)
+{
+    printk("hw_timer_init()\r\n");
+
+    // Set MFP Timer B to run at 36 Hz and trigger an interrupt on every tick
+    MEM(MFP_TBDR) = 0;         // Timer B counter max (i.e 255);
+    MEM(MFP_TBCR) = 0b0010111; // Timer B enabled, delay mode, /200 prescalar
+    MEM(MFP_VR) = 0x40;        // Set base vector for MFP interrupt handlers
+    MEM(MFP_IERA) = 0x01;      // Enable interrupts for Timer B
+    MEM(MFP_IMRA) = 0x01;      // Unmask interrupt for Timer B
+    
+    timer_interrupt = handler;
+
+    setup_irq(1, &mackerel_timer_irq);
+}
+
 
 void __init config_BSP(char *command, int len)
 {
     printk(KERN_INFO "\nMackerel 68k support by Colin Maykish <crmaykish@gmail.com>\n");
+
+    mach_sched_init = mackerel_timer_init;
 }
--- arch/m68k/platform/68000/mackerel/ints-mackerel.c
+++ arch/m68k/platform/68000/mackerel/ints-mackerel.c
@@ -1,22 +1,42 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <asm/traps.h>
+
+extern e_vector *_ramvec;
+
+static void intc_irq_enable(struct irq_data *d)
+{
+}
+
+static void intc_irq_disable(struct irq_data *d)
+{
+}
+
+static struct irq_chip intc_irq_chip = {
+	.name		= "MACKEREL-INTC",
+	.irq_enable	= intc_irq_enable,
+	.irq_disable	= intc_irq_disable,
+};
+
+asmlinkage irqreturn_t inthandler_vec1(void);
 
 void __init trap_init(void)
 {
     printk("trap_init()\r\n");
+
+    _ramvec[0x48] = (e_vector)inthandler_vec1;
 }
 
 void __init init_IRQ(void)
 {
+    int irq;
+
     printk("init_IRQ()\r\n");
-}
 
-void resume(void)
-{
-    printk("resume()\r\n");
-}
-
-void ret_from_exception(void)
-{
-    printk("ret_from_exception()\r\n");
+    for (irq = 0; (irq < NR_IRQS); irq++)
+    {
+        irq_set_chip_and_handler(irq, &intc_irq_chip, handle_simple_irq);
+    }
 }
